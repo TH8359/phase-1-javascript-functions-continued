902d93bdaad584b5be5d90ca14d4b9d1454b2b97
Tyler Hogue <hogue73@embarqmail.com>
2024-12-05 01:13:34 -0500

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+let message
+
+function saturdayFun(activity = "roller-skate"){
+message = `This Saturday, I want to ${activity}!`
+
+return message
+}
+
+const mondayWork = function (activity = "go to the office"){
+    message = `This Monday, I will ${activity}.`
+    return message
+}
+
+function wrapAdjective(highlight = "*"){
+   return function(adjective = "special"){
+        return `You are ${highlight}${adjective}${highlight}!`
+    }
+}
